401e13e5d04a99b3ec764f38baa152ad125b3487
Zachary Tong <zachary.tong@grabtaxi.com>
2020-02-13 16:34:50 +0800

    Pull country code from device locale

--- instant_news/lib/NewsBloc.dart
+++ instant_news/lib/NewsBloc.dart
@@ -2,12 +2,13 @@ import 'package:instant_news/NewsRepository.dart';
 import 'package:instant_news/model/Article.dart';
 import 'package:instant_news/model/NewsResponse.dart';
 import 'package:rxdart/rxdart.dart';
+import 'dart:ui' as ui;
 
 class NewsBloc {
   final NewsRepository repository = NewsRepository();
   final BehaviorSubject<List<Article>> _subject = BehaviorSubject<List<Article>>();
   final BehaviorSubject<String> _searchSubject = BehaviorSubject<String>();
-  final String countryCode = "SG";
+  final String countryCode = ui.window.locale.countryCode;
 
   NewsBloc() {
     _searchSubject.stream.debounceTime(Duration(milliseconds: 500))
